163a8be3ed4fed44bff0c158de04c954cf807754
bytefroster <bytefroster@gmail.com>
2026-05-05 13:41:23 +0600

    add setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "[+] Updating system..."
+sudo apt update && sudo apt upgrade -y
+
+echo "[+] Installing essentials..."
+sudo apt install -y git zsh curl wget
+
+echo "[+] Installing Oh My Zsh..."
+RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "[+] Installing Powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+echo "[+] Restoring config..."
+cp .zshrc ~/
+cp .p10k.zsh ~/ 2>/dev/null
+
+echo "[+] Setting default shell..."
+chsh -s $(which zsh)
+
+echo "[+] Done"
